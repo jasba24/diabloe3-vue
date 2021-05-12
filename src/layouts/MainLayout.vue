9b1e56f333853bd4a039b04eaddb6a1133806e20
jasba24 <johanbuitrago342@gmail.com>
2021-05-12 11:20:31 -0500

    add navbar and footer and the use of FontAwesome

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    <HeaderBar />
+
+    <router-view />
+
+    <FooterBar />
   </div>
 </template>
 
 <script>
+import HeaderBar from "@/components/HeaderBar/Index";
+import FooterBar from "@/components/FooterBar/Index";
+
 export default {
   name: "MainLayout",
+  components: {
+    HeaderBar,
+    FooterBar,
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,43 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
+);
+
 Vue.config.productionTip = false;
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
   router,
