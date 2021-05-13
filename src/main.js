78b4d39e1a2763ce0104fb3fcf38372edad4da66
jasba24 <johanbuitrago342@gmail.com>
2021-05-13 08:47:40 -0500

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,61 +1,26 @@
-import "@babel/polyfill";
-import "mutationobserver-shim";
-import Vue from "vue";
-import "./plugins/bootstrap-vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import "@babel/polyfill"
+import "mutationobserver-shim"
+import Vue from "vue"
+import "./plugins/bootstrap-vue"
+import "./plugins/FontAwesome"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-import {
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-} from "@fortawesome/free-solid-svg-icons";
-
-import {
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet,
-} from "@fortawesome/free-brands-svg-icons";
-
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-);
-
-Vue.config.productionTip = false;
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  methods: {
-    init() {
-      store.dispatch("oauth/getToken", null, { root: true });
-    },
-  },
+	router,
+	store,
+	methods: {
+		init() {
+			store.dispatch("oauth/getToken", null, { root: true })
+		},
+	},
 
-  created() {
-    this.init();
-  },
+	created() {
+		this.init()
+	},
 
-  render: (h) => h(App),
-}).$mount("#app");
+	render: h => h(App),
+}).$mount("#app")
