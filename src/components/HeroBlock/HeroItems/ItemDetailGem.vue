ab6079fe6043573aaaa2c8edd72b43593ab3d9ab
jasba24 <johanbuitrago342@gmail.com>
2021-05-14 16:38:03 -0500

    add items to hero

---
+++ src/components/HeroBlock/HeroItems/ItemDetailGem.vue
@@ -0,0 +1,48 @@
+<template>
+	<img :src="gemUrl" :alt="gem.item.name" :title="gem.item.name" />
+</template>
+
+<script>
+export default {
+	name: "GemSlotItem",
+	props: {
+		gem: {
+			required: true,
+			type: Object,
+		},
+	},
+
+	computed: {
+		gemUrl() {
+			const host = `http://media.blizzard.com/d3/icons/items/small/`
+			return `${host}${this.gem.item.icon}.png`
+		},
+	},
+}
+</script>
+
+<style lang="stylus">
+.d3-icon-item
+  min-height 100px
+  // El borde de la caja va determinar la rareza del objeto, segun el color que tenga
+  border-top-style solid
+  border-top-width 4px
+
+  &.item-none
+    border-color transparent
+
+  &.item-green
+    border-color #8bc34a
+
+  &.item-orange
+    border-color #ff9800
+
+  &.item-yellow
+    border-color #ffeb3b
+
+  &.item-blue
+    border-color #03a9f4
+
+  &.item-white
+    border-color #a0aab5
+</style>
---
+++ src/components/HeroBlock/HeroItems/index.vue
@@ -0,0 +1,137 @@
+<template>
+	<section class="hero-items mb-5">
+		<h2 class="font-diablo">Items</h2>
+
+		<hr />
+
+		<b-row>
+			<b-col cols="4" offset="4">
+				<item-detail :item="itemsData.head" />
+			</b-col>
+		</b-row>
+
+		<hr />
+
+		<b-row>
+			<b-col>
+				<item-detail :item="itemsData.shoulders" />
+			</b-col>
+			<b-col>
+				<item-detail :item="itemsData.torso" />
+			</b-col>
+			<b-col>
+				<item-detail :item="itemsData.neck" />
+			</b-col>
+		</b-row>
+
+		<hr />
+
+		<b-row>
+			<b-col>
+				<item-detail :item="itemsData.hands" />
+			</b-col>
+			<b-col>
+				<item-detail :item="itemsData.waist" />
+			</b-col>
+			<b-col>
+				<item-detail :item="itemsData.bracers" />
+			</b-col>
+		</b-row>
+
+		<hr />
+
+		<b-row>
+			<b-col>
+				<item-detail :item="itemsData.leftFinger" />
+			</b-col>
+			<b-col>
+				<item-detail :item="itemsData.legs" />
+			</b-col>
+			<b-col>
+				<item-detail :item="itemsData.rightFinger" />
+			</b-col>
+		</b-row>
+
+		<hr />
+
+		<b-row>
+			<b-col>
+				<item-detail :item="itemsData.mainHand" />
+			</b-col>
+			<b-col>
+				<item-detail :item="itemsData.feet" />
+			</b-col>
+			<b-col>
+				<item-detail :item="itemsData.offHand" />
+			</b-col>
+		</b-row>
+	</section>
+</template>
+
+<script>
+import ItemDetail from "./ItemDetail"
+
+const defaultItems = {
+	head: {
+		slotName: "head",
+	},
+	shoulders: {
+		slotName: "Shoulders",
+	},
+	torso: {
+		slotName: "Torso",
+	},
+	neck: {
+		slotName: "Neck",
+	},
+	hands: {
+		slotName: "Hands",
+	},
+	waist: {
+		slotName: "Waist",
+	},
+	bracers: {
+		slotName: "Bracers",
+	},
+	leftFinger: {
+		slotName: "Left Finger",
+	},
+	legs: {
+		slotName: "Legs",
+	},
+	rightFinger: {
+		slotName: "Right Finger",
+	},
+	mainHand: {
+		slotName: "Main Hand",
+	},
+	feet: {
+		slotName: "Feet",
+	},
+	offHand: {
+		slotName: "Off Hand",
+	},
+}
+
+export default {
+	name: "HeroItems",
+
+	components: { ItemDetail },
+
+	props: {
+		items: {
+			type: Object,
+			required: true,
+		},
+	},
+
+	computed: {
+		itemsData() {
+			return {
+				...defaultItems,
+				...this.items,
+			}
+		},
+	},
+}
+</script>
