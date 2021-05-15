a8c8709d72944947d97a29fd71969885c430abd8
jasba24 <johanbuitrago342@gmail.com>
2021-05-15 08:40:49 -0500

    complete about and error view

--- src/api/oauth.js
+++ src/api/oauth.js
@@ -1,22 +1,22 @@
-import { post } from "axios"
+import { post } from "axios";
 
-const clientId = process.env.VUE_APP_CLIENT_ID
-const clientSecret = process.env.VUE_APP_CLIENT_SECRET
+const clientId = process.env.VUE_APP_CLIENT_ID;
+const clientSecret = process.env.VUE_APP_CLIENT_SECRET;
 
-const region = "eu"
-const API_URL = `https://${region}.battle.net/oauth/token`
+const region = "eu";
+const API_URL = `https://${region}.battle.net/oauth/token`;
 
 function getToken() {
-	const body = new FormData()
+  const body = new FormData();
 
-	body.append("grant_type", "client_credentials")
+  body.append("grant_type", "client_credentials");
 
-	const config = {
-		headers: { "Content-Type": "multipart/form-data" },
-		auth: { username: clientId, password: clientSecret },
-	}
+  const config = {
+    headers: { "Content-Type": "multipart/form-data" },
+    auth: { username: clientId, password: clientSecret },
+  };
 
-	return post(`${API_URL}`, body, config)
+  return post(`${API_URL}`, body, config);
 }
 
-export { getToken }
+export { getToken };
